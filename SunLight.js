9a5b81337a266f811009b39159fa70075b1ec51a
Şan Güneş <sgunes@ee.oulu.fi>
2016-07-26 15:49:11 +0300

    first commit

---
+++ SunLight.js
@@ -0,0 +1,83 @@
+SunLight = function ( coordinates_, north_, east_, nadir_ ) {
+	THREE.Object3D.call( this );
+	this.type = "SunLight";
+
+	// Latitude and longtitude of the current location on the world
+	// Measured as decimal degrees. North and east is positive
+	this.coordinates = new THREE.Vector2();
+	this.coordinates.copy( coordinates_ );
+
+	// The unit vector that is pointing the north in the scene
+	this.north = new THREE.Vector3();
+	this.north.copy( north_ );
+
+	// The unit vector that is pointing the east in the scene
+	this.east = new THREE.Vector3();
+	this.east.copy( east_ );
+
+	// The unit vector that is pointing the ground in the scene, same as gravity
+	this.nadir = new THREE.Vector3();
+	this.nadir.copy( nadir_ );
+
+	// The azimuth of the sun. Starts from the north, clockwise. In radians.
+	this.azimuth = 0.0;
+	// The elevation of the sun. Starts from the horizon. In radians.
+	this.elevation = 0.0;
+
+	// Local date and time
+	this.localDate = new Date();
+
+	// The directional light in Three.js is managed by a directional vector.
+	// To make life easier, I'm adding the light as a child to this hinge object
+	// and rotating this object in order to set the light's direction
+	this.hingeObject = new THREE.Object3D();
+	this.add( this.hingeObject );
+
+	// The directional light which is used as the sun light
+	this.directionalLight = new THREE.DirectionalLight(); 
+	this.directionalLight.castShadow = true;
+	this.hingeObject.add( this.directionalLight );
+};
+
+SunLight.prototype = Object.assign(
+	Object.create( THREE.Object3D.prototype ),
+	{
+		constructor: SunLight,
+
+		toJSON: function ( meta ) {
+			var data = THREE.Object3D.prototype.toJSON.call( this, meta );
+			// TODO
+			// not implemented yet
+			return data;
+		}
+	} );
+
+// Updates the orientation of the sun using the coordinates and the localDate
+SunLight.prototype.updateOrientation = function ( update_date_ = true ) {
+	// Update the local date if the parameter is true (true by default).
+	if ( update_date_ ) { 
+		this.localDate = new Date();
+	}
+	// TODO
+	// Just add some random values for now for demonstration purposes
+	// 26.07.2016 16:30 in Helsinki
+	this.azimuth = this._degreesToRadians( 254.07 );
+	this.elevation = this._degreesToRadians( 31.01 );
+}
+
+// Updates the directional light based on the sun's orientation and the north
+// vector. This is actually done by rotating the hinge object which is the
+// parent of the directional light.
+SunLight.prototype.updateDirectionalLight = function () {
+	this.directionalLight.position.copy( this.north );
+	var rotator = new THREE.Quaternion();
+	rotator.setFromAxisAngle( this.east, this.elevation );
+	this.hingeObject.quaternion.premultiply( rotator );
+	rotator.setFromAxisAngle( this.nadir, this.azimuth );
+	this.hingeObject.quaternion.premultiply( rotator );
+}
+
+SunLight.prototype._degreesToRadians = function ( degrees_ ) {
+	return ( degrees_ % 360.0 ) * Math.PI / 180.0;
+}
+
---
+++ example-scene.js
@@ -0,0 +1,80 @@
+var scene, root, renderer, camera, controls;
+init();
+createScene();
+animate();
+
+function init () {
+	scene = new THREE.Scene();
+	root = new THREE.Object3D();
+	scene.add( root );
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.shadowMap.enabled = true;
+	document.body.appendChild( renderer.domElement );
+	camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000.0
+	);
+	camera.position.z = 50.0;
+	controls = new THREE.OrbitControls( camera );
+	controls.addEventListener( "change", render );
+}
+
+function animate () {
+	requestAnimationFrame( animate );
+	controls.update();
+	render();
+}
+
+function render () {
+	renderer.render( scene, camera );
+}
+
+function createScene () {
+	// Create the objects
+	// In this case z+ is north, y- is the gravity.
+	var ground = createBox( 50.0, 1.0, 50.0, 0x99ff99 );
+	ground.position.set( 0.0, 0.0, 0.0 );
+	root.add( ground );
+	// Cube in the middle
+	var cube = createBox( 5.0, 5.0, 5.0 );
+	cube.position.set( 0.0, 5.0, 0.0 );
+	root.add( cube );
+	// North pole indicator
+	var northIndicator = createBox( 2.0, 2.0, 8.0, 0xff0000 );
+	northIndicator.position.set( 0.0, 1.0, 20.0 );
+	root.add( northIndicator );
+
+	// Add an ambient light
+	root.add( new THREE.AmbientLight( 0x333333) );
+
+	// Create the sun light and add it to the scene
+	var sunLight = new SunLight(
+		new THREE.Vector2( 0.0, 0.0 ),
+		new THREE.Vector3( 0.0, 0.0, 1.0 ),
+		new THREE.Vector3( -1.0, 0.0, 0.0 ),
+		new THREE.Vector3( 0.0, -1.0, 0.0 )
+	);
+	root.add( sunLight );
+
+	sunLight.updateOrientation();
+	sunLight.updateDirectionalLight();
+
+	// Adjust the directional light's shadow camera dimensions
+	sunLight.directionalLight.shadow.camera.right = 30.0;
+	sunLight.directionalLight.shadow.camera.left = -30.0;
+	sunLight.directionalLight.shadow.camera.top = 30.0;
+	sunLight.directionalLight.shadow.camera.bottom = -30.0;
+}
+
+function createBox ( width_, height_, depth_, color_ = 0xffffff ) {
+	var geometry = new THREE.BoxGeometry( width_, height_, depth_ );
+	var material = new THREE.MeshPhongMaterial( { color: color_ } );
+	var cube = new THREE.Mesh( geometry, material );
+	cube.castShadow = true;
+	cube.receiveShadow = true;
+	return cube;
+}
+
