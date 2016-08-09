709e73bcebc4d7383b27cec88bb2d41fa4877b3c
Şan Güneş <sgunes@ee.oulu.fi>
2016-08-09 09:56:11 +0300

    moved solar orientation calculation code into SunLight

--- SunLight.js
+++ SunLight.js
@@ -59,7 +59,9 @@ SunLight.prototype.updateOrientation = function ( update_date_ = true ) {
 		this.localDate = new Date();
 	}
 
-	var sunOrientation = getAzEl(
+	var solarOrientationCalculator = new this.SolarOrientationCalculator();
+
+	var sunOrientation = solarOrientationCalculator.getAzEl(
 			this.coordinates.x,
 			this.coordinates.y,
 			this.localDate
@@ -82,5 +84,307 @@ SunLight.prototype.updateDirectionalLight = function () {
 
 SunLight.prototype._degreesToRadians = function ( degrees_ ) {
 	return ( degrees_ % 360.0 ) * Math.PI / 180.0;
+}
+
+// ---
+// Methods for calculating the Sun's orientation go below
+// ---
+
+SunLight.prototype.SolarOrientationCalculator = function() {
+	this.a = "some val";
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype.getAzEl =
+	function( lat_, lon_, date_ = new Date() )
+{
+	var jday = this._getJD( date_ );
+	var tl = this._getTimeLocal( date_ );
+	var tz = date_.getTimezoneOffset() / -60;
+	var dst = true;
+	var total = jday + tl/1440.0 - tz/24.0;
+	var T = this._calcTimeJulianCent( total );
+	sunOrientation = this._calcAzEl( false, T, tl, lat_, lon_, tz );
+	return sunOrientation;
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._getJD = 
+	function( date_ = new Date() )
+{
+	var docmonth = date_.getMonth() + 1;
+	var docday = date_.getDate();
+	var docyear = date_.getFullYear();
+	if ( (this._isLeapYear(docyear)) && (docmonth == 2) ) {
+		if (docday > 29) {
+			docday = 29;
+		} 
+	} else {
+		// 1900 is a known non-leap year
+		if (docday > new Date(1900, docmonth, 0).getDate()) {
+			docday = new Date(1900, docmonth, 0).getDate();
+		}
+	}
+	if (docmonth <= 2) {
+		docyear -= 1;
+		docmonth += 12;
+	}
+	var A = Math.floor(docyear/100);
+	var B = 2 - A + Math.floor(A/4);
+	var JD = Math.floor(365.25*(docyear + 4716)) + 
+		Math.floor(30.6001*(docmonth+1)) + docday + B - 1524.5;
+	return JD;
+}
+
+// Returns the current time in minutes without the DST
+SunLight.prototype.SolarOrientationCalculator.prototype._getTimeLocal = 
+	function( date_ = new Date() )
+{
+	var totalMinutes = 0.0;
+	totalMinutes += 60.0 * date_.getHours();
+	// TODO
+	// Remove one hour if DST is in effect
+	totalMinutes += date_.getMinutes();
+	totalMinutes += date_.getSeconds() / 60.0;
+	return totalMinutes;
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcTimeJulianCent =
+	function( jd )
+{
+	var T = (jd - 2451545.0)/36525.0;
+	return T;
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcAzEl =
+	function( output, T, localtime, latitude, longitude, zone )
+{
+	var result = { "azimuth": 0.0, "elevation": 0.0 };
+	var eqTime = this._calcEquationOfTime(T);
+	var theta  = this._calcSunDeclination(T);
+	var solarTimeFix = eqTime + 4.0 * longitude - 60.0 * zone;
+	var earthRadVec = this._calcSunRadVector(T);
+	var trueSolarTime = localtime + solarTimeFix;
+	while (trueSolarTime > 1440)
+	{
+		trueSolarTime -= 1440;
+	}
+	var hourAngle = trueSolarTime / 4.0 - 180.0;
+	if (hourAngle < -180) 
+	{
+		hourAngle += 360.0;
+	}
+	var haRad = this._degToRad(hourAngle);
+	var csz = Math.sin(this._degToRad(latitude)) *
+		Math.sin(this._degToRad(theta)) + Math.cos(this._degToRad(latitude)) *
+		Math.cos(this._degToRad(theta)) * Math.cos(haRad);
+	if (csz > 1.0) 
+	{
+		csz = 1.0;
+	} else if (csz < -1.0) 
+	{ 
+		csz = -1.0;
+	}
+	var zenith = this._radToDeg(Math.acos(csz));
+	var azDenom = ( Math.cos(this._degToRad(latitude)) *
+		Math.sin(this._degToRad(zenith)) );
+	if (Math.abs(azDenom) > 0.001) {
+		azRad = (( Math.sin(this._degToRad(latitude)) *
+			Math.cos(this._degToRad(zenith)) ) -
+			Math.sin(this._degToRad(theta))) / azDenom;
+		if (Math.abs(azRad) > 1.0) {
+			if (azRad < 0) {
+				azRad = -1.0;
+			} else {
+				azRad = 1.0;
+			}
+		}
+		var azimuth = 180.0 - this._radToDeg(Math.acos(azRad))
+		if (hourAngle > 0.0) {
+			azimuth = -azimuth;
+		}
+	} else {
+		if (latitude > 0.0) {
+			azimuth = 180.0;
+		} else { 
+			azimuth = 0.0;
+		}
+	}
+	if (azimuth < 0.0) {
+		azimuth += 360.0;
+	}
+	var exoatmElevation = 90.0 - zenith;
+
+	// Atmospheric Refraction correction
+
+	if (exoatmElevation > 85.0) {
+		var refractionCorrection = 0.0;
+	} else {
+		var te = Math.tan(this._degToRad(exoatmElevation));
+		if (exoatmElevation > 5.0) {
+			var refractionCorrection = 58.1 / te - 0.07 / (te*te*te) +
+				0.000086 / (te*te*te*te*te);
+		} else if (exoatmElevation > -0.575) {
+			var refractionCorrection = 1735.0 + exoatmElevation *
+				(-518.2 + exoatmElevation * (103.4 + exoatmElevation *
+					(-12.79 + exoatmElevation * 0.711) ) );
+		} else {
+			var refractionCorrection = -20.774 / te;
+		}
+		refractionCorrection = refractionCorrection / 3600.0;
+	}
+
+	var solarZen = zenith - refractionCorrection;
+
+	result.azimuth = Math.floor(azimuth*100 +0.5)/100.0;
+	result.elevation = Math.floor((90.0-solarZen)*100+0.5)/100.0;
+	return result;
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._isLeapYear = 
+	function( yr )
+{
+  return ((yr % 4 == 0 && yr % 100 != 0) || yr % 400 == 0);
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._radToDeg =
+	function( angleRad )
+{
+  return (180.0 * angleRad / Math.PI);
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._degToRad =
+	function( angleDeg )
+{
+  return (Math.PI * angleDeg / 180.0);
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcEquationOfTime =
+	function( t )
+{
+	var epsilon = this._calcObliquityCorrection(t);
+	var l0 = this._calcGeomMeanLongSun(t);
+	var e = this._calcEccentricityEarthOrbit(t);
+	var m = this._calcGeomMeanAnomalySun(t);
+
+	var y = Math.tan(this._degToRad(epsilon)/2.0);
+	y *= y;
+
+	var sin2l0 = Math.sin(2.0 * this._degToRad(l0));
+	var sinm   = Math.sin(this._degToRad(m));
+	var cos2l0 = Math.cos(2.0 * this._degToRad(l0));
+	var sin4l0 = Math.sin(4.0 * this._degToRad(l0));
+	var sin2m  = Math.sin(2.0 * this._degToRad(m));
+
+	var Etime = y * sin2l0 - 2.0 * e * sinm + 4.0 * e * y * sinm * cos2l0 -
+		0.5 * y * y * sin4l0 - 1.25 * e * e * sin2m;
+	return this._radToDeg(Etime)*4.0; // in minutes of time
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcSunDeclination =
+	function( t )
+{
+	var e = this._calcObliquityCorrection(t);
+	var lambda = this._calcSunApparentLong(t);
+
+	var sint = Math.sin(this._degToRad(e)) * Math.sin(this._degToRad(lambda));
+	var theta = this._radToDeg(Math.asin(sint));
+	return theta; // in degree
+}
+
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcSunRadVector =
+	function( t )
+{
+	var v = this._calcSunTrueAnomaly(t);
+	var e = this._calcEccentricityEarthOrbit(t);
+	var R = (1.000001018 * (1 - e * e)) /
+		(1 + e * Math.cos(this._degToRad(v)));
+	return R; // in AU
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcObliquityCorrection =
+	function( t )
+{
+	var e0 = this._calcMeanObliquityOfEcliptic(t);
+	var omega = 125.04 - 1934.136 * t;
+	var e = e0 + 0.00256 * Math.cos(this._degToRad(omega));
+	return e; // in degree
+}
+
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcSunApparentLong =
+	function( t )
+{
+	var o = this._calcSunTrueLong(t);
+	var omega = 125.04 - 1934.136 * t;
+	var lambda = o - 0.00569 - 0.00478 * Math.sin(this._degToRad(omega));
+	return lambda; // in degrees
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcGeomMeanLongSun = 
+	function(t)
+{
+	var L0 = 280.46646 + t * (36000.76983 + t*(0.0003032));
+	while(L0 > 360.0)
+	{
+		L0 -= 360.0;
+	}
+	while(L0 < 0.0)
+	{
+		L0 += 360.0;
+	}
+	return L0; // in degrees
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcEccentricityEarthOrbit = 
+	function(t)
+{
+	var e = 0.016708634 - t * (0.000042037 + 0.0000001267 * t);
+	return e; // unitless
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcGeomMeanAnomalySun = 
+	function(t)
+{
+	var M = 357.52911 + t * (35999.05029 - 0.0001537 * t);
+	return M; // in degrees
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcSunTrueAnomaly = 
+	function(t)
+{
+	var m = this._calcGeomMeanAnomalySun(t);
+	var c = this._calcSunEqOfCenter(t);
+	var v = m + c;
+	return v; // in degrees
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcMeanObliquityOfEcliptic = 
+	function(t)
+{
+	var seconds = 21.448 - t*(46.8150 + t*(0.00059 - t*(0.001813)));
+	var e0 = 23.0 + (26.0 + (seconds/60.0))/60.0;
+	return e0; // in degrees
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcSunTrueLong = 
+	function(t)
+{
+	var l0 = this._calcGeomMeanLongSun(t);
+	var c = this._calcSunEqOfCenter(t);
+	var O = l0 + c;
+	return O; // in degrees
+}
+
+SunLight.prototype.SolarOrientationCalculator.prototype._calcSunEqOfCenter = 
+	function(t)
+{
+	var m = this._calcGeomMeanAnomalySun(t);
+	var mrad = this._degToRad(m);
+	var sinm = Math.sin(mrad);
+	var sin2m = Math.sin(mrad+mrad);
+	var sin3m = Math.sin(mrad+mrad+mrad);
+	var C = sinm * (1.914602 - t * (0.004817 + 0.000014 * t)) + sin2m *
+		(0.019993 - 0.000101 * t) + sin3m * 0.000289;
+	return C; // in degrees
 }
 
